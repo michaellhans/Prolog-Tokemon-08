22dff284806c625d78ea1272696a3a34bb054b93
Florencia Wijaya <florenciawijaya11@gmail.com>
2019-11-10 20:22:06 +0700

    inventory

---
+++ inventory.pl
@@ -0,0 +1,11 @@
+/* checklength digunakan untuk mengecek banyaknya tokemon yang ada pada inventory */
+checklength([],0).
+checklength([_|T],N) :- checklength(T,N1), N is N1+1.
+
+/* deltokemon digunakan untuk menghapus tokemon dari list */
+deltokemon(X,[X|T],T).
+deltokemon(X,[A|T],[A|B]) :- deltokemon(X,T,B).
+
+/* addtokemon digunakan untuk menambahkan tokemon ke dalam list */
+addtokemon(X,[],[X]).
+addtokemon(X,[H|T],[H|A]) :- addtokemon(X,T,A).
