64b957c98e8f3f3cc3b974f0bee395033f1e846b
Lionnarta <lionnarta.savirandy2000@gmail.com>
2019-11-10 21:43:16 +0700

    update

--- inventory.pl
+++ inventory.pl
@@ -10,6 +10,19 @@ deltokemon(X,[A|T],[A|B]) :- deltokemon(X,T,B).
 addtokemon(X,[],[X]).
 addtokemon(X,[H|T],[H|A]) :- addtokemon(X,T,A).
 
-/* printlist digunakan untuk menampilkan isi list */
-printlist([]).
-printlist([X|List]) :- write(X),nl,printlist(List).
+/* dynamiclist */
+:- dynamic(inventory/6).
+inventory([(refflesia,115,20,leaves,woodhammer,8),(sijagokuning,95,25,fire,flamewheel,7)]).
+
+/* printinventory */
+printinventory(inventory([(Name,Health,_,Type,_,_)])) :-
+        write('Name     : '),write(Name),nl,
+        write('Health   : '),write(Health),nl,
+        write('Type     : '),write(Type),nl.
+printinventory(inventory([(Name,Health,_,Type,_,_)|T])) :-
+        write('Name     : '),write(Name),nl,
+        write('Health   : '),write(Health),nl,
+        write('Type     : '),write(Type),nl,
+        printinventory(inventory(T)).
+
+status :- 
