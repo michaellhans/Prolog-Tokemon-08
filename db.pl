ae7af8ee262ab161527eb015886ed63f334d746c
michaelhans <michaelhans777@gmail.com>
2019-11-11 11:29:33 +0700

    Update Attack dan Skill

--- db.pl
+++ db.pl
@@ -25,6 +25,94 @@ tokemon(wush,110,15,wind,roost,21).
 tokemon(aang,115,16,wind,skyattack,22).
 tokemon(topan,120,17,wind,aerialace,23).
 
+skill(water,hydropump). % damage = 50
+skill(earth,earthquake). % damage = 75
+skill(wind,roost). % health +20
+skill(leaves,absorb). % health +(200%)*damage
+skill(lightning,bolt). % damage = 90
+skill(fire,eruption). % damage = 110. syarat = abis pake skill ini, damage berkurang menjadi 60% nya
+
+activateSkill(NameSkill) :-
+    ((NameSkill == hydropump) -> write('You use hydropump skill'),nl,
+    retract(enemy(Name2,Hp2,Dmg2,Type2,Skill2,Id2)),
+    NewHp2 is Hp2 - 50,
+    write('Enemy is taken heavy damage!'),nl,
+    (NewHp2 =< 0 -> Dead is 0,
+    write('Enemy is down!'),nl,
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(Dead),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,Dead,Dmg2,Type2,Skill2,Id2));
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(NewHp2),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,NewHp2,Dmg2,Type2,Skill2,Id2)));
+
+    (NameSkill == earthquake) -> write('You use earthquake skill'),nl,
+    retract(enemy(Name2,Hp2,Dmg2,Type2,Skill2,Id2)),
+    NewHp2 is Hp2 - 75,
+    write('Enemy is taken heavy damage!'),nl,
+    (NewHp2 =< 0 -> Dead is 0,
+    write('Enemy is down!'),nl,
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(Dead),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,Dead,Dmg2,Type2,Skill2,Id2));
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(NewHp2),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,NewHp2,Dmg2,Type2,Skill2,Id2)));
+    
+    (NameSkill == roost) -> write('You use roost skill'),nl,
+    retract(me(Name1,Hp1,Dmg1,Type1,Skill1,Id1)),
+    NewHp1 is Hp1 + 20,
+    write('Skill Activated!'),nl,
+    write('Tokemonku - '), write(Name1),nl,
+    write('Health : '), write(NewHp1),nl,
+    write('Type : '),write(Type1),nl,nl,
+    assertz(me(Name1,NewHp1,Dmg1,Type1,Skill1,Id1));
+
+    (NameSkill == absorb) -> write('You use absorb skill'),nl,
+    retract(me(Name1,Hp1,Dmg1,Type1,Skill1,Id1)),
+    NewHp1 is Hp1 + 2*Dmg1,
+    write('Skill Activated!'),nl,
+    write('Tokemonku - '), write(Name1),nl,
+    write('Health : '), write(NewHp1),nl,
+    write('Type : '),write(Type1),nl,nl,
+    assertz(me(Name1,NewHp1,Dmg1,Type1,Skill1,Id1));
+
+    (NameSkill == bolt) -> write('You use bolt skill'),nl,
+    retract(enemy(Name2,Hp2,Dmg2,Type2,Skill2,Id2)),
+    NewHp2 is Hp2 - 90,
+    write('Enemy is taken heavy damage!'),nl,
+    (NewHp2 =< 0 -> Dead is 0,
+    write('Enemy is down!'),nl,
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(Dead),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,Dead,Dmg2,Type2,Skill2,Id2));
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(NewHp2),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,NewHp2,Dmg2,Type2,Skill2,Id2)));
+
+    (NameSkill == eruption) -> write('You use eruption skill'),nl,
+    retract(enemy(Name2,Hp2,Dmg2,Type2,Skill2,Id2)),
+    NewHp2 is Hp2 - 110,
+    write('Enemy is taken heavy damage!'),nl,
+    (NewHp2 =< 0 -> Dead is 0,
+    write('Enemy is down!'),nl,
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(Dead),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,Dead,Dmg2,Type2,Skill2,Id2));
+    write('Musuh - '),write(Name2),nl,
+    write('Health : '),write(NewHp2),nl,
+    write('Type : '),write(Type2),nl,nl,
+    assertz(enemy(Name2,NewHp2,Dmg2,Type2,Skill2,Id2)));
+
+    write('You have no skill to use'),nl).
+
 /* Legendary Tokemon */
 /* legendary(name) */
 legendary(ligator).
