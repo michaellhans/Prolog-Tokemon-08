bd603ee047c6de5e624fcc76e500e694aa83650f
Lionnarta <lionnarta.savirandy2000@gmail.com>
2019-11-11 14:03:57 +0700

    Update inventory.pl

--- inventory.pl
+++ inventory.pl
@@ -10,19 +10,42 @@ deltokemon(X,[A|T],[A|B]) :- deltokemon(X,T,B).
 addtokemon(X,[],[X]).
 addtokemon(X,[H|T],[H|A]) :- addtokemon(X,T,A).
 
-/* dynamiclist */
+/* dynamicinventory dan dynamicenemy */
 :- dynamic(inventory/6).
 inventory(refflesia,115,20,leaves,woodhammer,8).
 inventory(sijagokuning,95,25,fire,flamewheel,7).
 
+:- dynamic(enemy/6).
+enemy(ligator,630,40,water,hydropump,1).
+enemy(camelia,580,57,leaves,leafstorm,2).
+enemy(phoenix,500,70,fire,blastburn,3).
+
 /* printinventory */
+printinventory([]).
+printinventory([H|T]) :-
+        write(H),nl,
+        inventory(H,Health,_,Type,_,_),
+        write('Health   : '),write(Health),nl,
+        write('Type     : '),write(Type),nl,nl,
+        printinventory(T).
+
+/* enemy */
+printenemy([]).
+printenemy([H|T]) :-
+        write(H),nl,
+        enemy(H,Health,_,Type,_,_),
+        write('Health   : '),write(Health),nl,
+        write('Type     : '),write(Type),nl,nl,
+        printenemy(T).
+
+/* status */
 status :-
-        findall(Name,inventory(Name,Health,_,Type,_,_),Result),
-        write(Result),nl,
-        findall(Health,inventory(Name,Health,_,Type,_,_),Result1),
-        write('Health   : '),write(Result1),nl,
-        findall(Type,inventory(Name,Health,_,Type,_,_),Result2),
-        write('Type     : '),write(Result2),nl.
+        findall(X,inventory(X,_,_,_,_,_),Result),
+        write('Your Tokemon:'),nl,
+        printinventory(Result),
+        findall(X,enemy(X,_,_,_,_,_),Result1),
+        write('Your Enemy:'),nl,
+        printenemy(Result1).
 
 /* removefromlist digunakan untuk menghapus tokemon dari inventory */
 removefromlist(X) :- retract(inventory(OldList)), deltokemon(X,OldList,NewList), assertz(inventory(NewList)).
