edd1f01d8a565e26fa046cced78dc0ed9c8d41b4
Florencia Wijaya <florenciawijaya11@gmail.com>
2019-11-15 18:26:47 +0700

    rapiin & comment

--- inventory.pl
+++ inventory.pl
@@ -1,14 +1,27 @@
-/* dynamicinventory dan dynamicenemy */
+/* Deklarasi Fakta */
+/* inventory adalah dynamic predicate yang menampung seluruh tokemon yang dimiliki player */
 :- dynamic(inventory/6).
 inventory(refflesia,115,20,leaves,woodhammer,8).
 inventory(sijagokuning,95,25,fire,eruption,7).
 
+/* listenemy adalah dynamic predicate yang menampung seluruh Legendary Tokemon */
 :- dynamic(listenemy/6).
 listenemy(ligator,630,40,water,hydropump,1).
 listenemy(camelia,580,57,leaves,leafstorm,2).
 listenemy(phoenix,500,70,fire,blastburn,3).
 
-/* printinventory */
+/* Kondisi inventory */
+/* isfull adalah dynamic predicate yang menampung jumlah elemen pada inventory player */
+:- dynamic(isfull/1).
+isfull(2).
+
+/* Kondisi menang */
+:- dynamic(iswin/1).
+iswin(3).
+
+
+/* Deklarasi Rules */
+/* printinventory digunakan untuk menampilkan listing tokemon yang dimiliki */
 printinventory([]).
 printinventory([H|T]) :-
         write(H),nl,
@@ -17,7 +30,7 @@ printinventory([H|T]) :-
         write('Type     : '),write(Type),nl,nl,
         printinventory(T).
 
-/* enemy */
+/* printenemy digunakan untuk menampilkan listing enemy (Legendary Tokemon) */
 printenemy([]).
 printenemy([H|T]) :-
         write(H),nl,
@@ -26,18 +39,24 @@ printenemy([H|T]) :-
         write('Type     : '),write(Type),nl,nl,
         printenemy(T).
 
-/* status */
+/* status untuk menampilkan Tokemon yang dimiliki oleh player, beserta propertinya */
 status :-
         command(initstart,A),
-        ((A=:=0 -> write('You even have not started the game yet.'),nl);
-        (A=:=1 ->
-        findall(X,inventory(X,_,_,_,_,_),Result),
-        write('Your Tokemon:'),nl,
-        printinventory(Result),
-        findall(X,listenemy(X,_,_,_,_,_),Result1),
-        write('Your Enemy:'),nl,
-        printenemy(Result1))).
+        (
+                (A=:=0 -> 
+                        write('You even have not started the game yet.'),nl
+                );
+                (A=:=1 ->
+                        findall(X,inventory(X,_,_,_,_,_),Result),
+                        write('Your Tokemon:'),nl,
+                        printinventory(Result),
+                        findall(X,listenemy(X,_,_,_,_,_),Result1),
+                        write('Your Enemy:'),nl,
+                        printenemy(Result1)
+                )
+        ).
 
+/* healInventory untuk menampilkan inventory player setelah semua tokemon di-heal */
 healinventory([]).
 healinventory([H|T]) :-
         write(H),nl,
@@ -50,25 +69,35 @@ healinventory([H|T]) :-
         assertz(inventory(H,NewHealth,Damage,Type,Skill,Id)),
         healinventory(T).
 
+/* heal untuk menambah semua Health pada tokemon yang dimiliki oleh player */
 heal :-
         command(initstart,A),
-        ((A=:=0 -> write('You even have not started the game yet.'),nl);
-        (A=:=1 -> 
-        playerposition(PosX,PosY),
-        (\+gym(PosX,PosY) -> write('You cannot heal your tokemon outside Gym!'),nl;
-        command(initheal,X),
-        ((X =:= 0 -> retract(command(initheal,0)),
-        assertz(command(initheal,1)),
-        findall(Y,inventory(Y,_,_,_,_,_),Result),
-        write('You use your only one gym ticket!'),nl,
-        write('All of your tokemons have been recovered!'),nl,
-        write('Your Tokemon:'),nl,
-        healinventory(Result));
-        (X=:=1 -> write('You do not have a ticket to the Gym!'),nl))))).
-        
-/* Kondisi inventory */
-:- dynamic(isfull/1).
-isfull(2).
+        (
+                (A=:=0 -> 
+                        write('You even have not started the game yet.'),nl);
+                (A=:=1 -> 
+                        playerposition(PosX,PosY),
+                        (\+gym(PosX,PosY) -> 
+                                write('You cannot heal your tokemon outside Gym!'),nl;
+                        command(initheal,X),
+                        (
+                                (X =:= 0 -> 
+                                        retract(command(initheal,0)),
+                                        assertz(command(initheal,1)),
+                                        findall(Y,inventory(Y,_,_,_,_,_),Result),
+                                        write('You use your only one gym ticket!'),nl,
+                                        write('All of your tokemons have been recovered!'),nl,
+                                        write('Your Tokemon:'),nl,
+                                        healinventory(Result));
+                                (X=:=1 -> 
+                                        write('You do not have a ticket to the Gym!'),nl
+                                )
+                        )
+                        )
+                )
+        ).
+
+/* capture digunakan untuk memasukkan Tokemon yang telah dikalahkan ke dalam inventory player */
 
 capture :-
         command(initstart,A),
@@ -96,7 +125,7 @@ capture :-
                 )
         ).
 
-/* pick and drop */
+/* pick digunakan untuk memilih Tokemon yang akan dipakai saat fight */
 pick(X) :-
         command(initstart,A),
         command(initfight,B),
@@ -110,6 +139,7 @@ pick(X) :-
         write(' I choose you!'),nl),
         retract(command(initpick,0)),assertz(command(initpick,1))).
 
+/* drop digunakan untuk melepaskan Tokemon yang ada di inventory jika inventory sudah penuh */
 drop(X) :-
         command(initstart,A),
         ((A=:=0 -> write('You even have not started the game yet.'),nl);
@@ -125,8 +155,3 @@ drop(X) :-
         assertz(isfull(NewC)));
         (Response==no,!));
         (write('You dont have '),write(X),nl)))).
-
-/* Kondisi menang */
-:- dynamic(iswin/1).
-iswin(3).
-
--- test.pl
+++ test.pl
@@ -1,3 +1,4 @@
+/* Deklarasi Fakta */
 positionX(1).
 positionX(2).
 positionX(3).
@@ -36,18 +37,22 @@ run(3).
 run(4).
 run(5).
 
+/* Deklarasi Rules */
+/* randomRun untuk generate kesempatan untuk run */
 randomRun(LowerBound,UpperBound,R) :-
     random(LowerBound,UpperBound,R),
     run(R),
     R >= LowerBound,
     R =< UpperBound, !.
 
+/* randomId untuk generate tokemon lawan yang muncul */
 randomId(LowerBound,UpperBound,R):-
     random(LowerBound,UpperBound,R),
     tokemon(_,_,_,_,_,R),
     R >= LowerBound,
     R =< UpperBound,!.
 
+/* checkPerimeter untuk mengecek tokemon yang muncul */
 checkPerimeter :-
     (isLegendaryAppear);
     (isTokemonAppear);
@@ -72,35 +77,46 @@ do(_):- write('Invalid Command').
 
 have(X):- X==nani,!.
 
+/* fightLegend untuk assert Legendary ke dynamic predicate enemy */
 fightLegend(Id) :-
     tokemon(Name,Hp,Dmg,Type,Skill,Id),
     assertz(enemy(Name,Hp,Dmg,Type,Skill,Id)).
 
+/* fightNormal untuk assert Normal ke dynamic predicate enemy */
 fightNormal(Id) :-
     tokemon(Name,Hp,Dmg,Type,Skill,Id),
     assertz(enemy(Name,Hp,Dmg,Type,Skill,Id)).
 
+/* isLegendaryAppear adalah mekanisme jika bertemu dengan Legendary Tokemon */
 isLegendaryAppear :-
     command(initfight,A),
-    ((A=:=0 -> 
-    playerposition(Xpos,Ypos),
-    \+gym(Xpos,Ypos),
-    random(1,10,X),
-    X =:= 5,
-    randomId(1,4,Id),
-    tokemon(Name,_,_,_,_,Id),nl,
-    write('Oh No! Legendary Tokemon Appeared!'),nl,
-    write('It is an '), write(Name), nl,
-    retract(command(inittokemonappear,0)),assertz(command(inittokemonappear,1)),
-    write('Fight or Run?'),nl,
-    read(Response),nl,
-    ((Response == run) -> isLegendaryRun(Id);
-    (Response == fight) -> write('What a legend! Here you go'),nl,fightLegend(Id),fight;
-    write('Please input the right response!')),nl,!)).
+    (
+        /* Jika dalam keadaan akan bertarung */
+        (A=:=0 -> 
+            playerposition(Xpos,Ypos),
+            /* Tidak berada di dalam gym */
+            \+gym(Xpos,Ypos),
+            /* Peluang = 10% */
+            random(1,101,X),
+            X > 90,
+            randomId(1,4,Id),
+            tokemon(Name,_,_,_,_,Id),nl,
+            write('Oh No! Legendary Tokemon Appeared!'),nl,
+            write('It is an '), write(Name), nl,
+            retract(command(inittokemonappear,0)),assertz(command(inittokemonappear,1)),
+            write('Fight or Run?'),nl,
+            read(Response),nl,
+            ((Response == run) -> isLegendaryRun(Id);
+            (Response == fight) -> write('What a legend! Here you go'),nl,fightLegend(Id),fight;
+            write('Please input the right response!')),nl,!)
+    ).
 
+/* isTokemonAppear adalah mekanisme jika bertemu dengan Normal Tokemon */
 isTokemonAppear :-
+    /* Jika tidak ada Legendary Tokemon yang muncul */
     \+isLegendaryAppear,
     playerposition(Xpos,Ypos),
+    /* Tidak berada di dalam gym */
     \+gym(Xpos,Ypos),
     random(1,21,X),
     X >= 1,
@@ -116,14 +132,36 @@ isTokemonAppear :-
     (Response == fight) -> write('What a legend! Here you go'),nl,fightNormal(Id),fight;
     write('Please input the right response!'),nl),!.
 
+/* isTokemonRun adalah mekanisme jika memilih run dari Normal Tokemon */
 isTokemonRun(Id) :-
     randomRun(1,3,R),
-    ((R =:= 1 -> write('Lucky you, you successfully escaped the wild Tokemon!'),
-    retract(command(inittokemonappear,1)),assertz(command(inittokemonappear,0)));
-    write('Poor you, you had to fight the wild Tokemon!'),nl,fightNormal(Id),fight),!.
+    /* Peluangnya 1:2 */
+    (
+        /* Mekanisme jika berhasil run */
+        (R =:= 1 -> 
+            write('Lucky you, you successfully escaped the wild Tokemon!'),
+            retract(command(inittokemonappear,1)),
+            assertz(command(inittokemonappear,0))
+        );
+        /* Mekanisme jika gagal run */
+            write('Poor you, you had to fight the wild Tokemon!'),nl,
+            fightNormal(Id),
+            fight
+    ),!.
 
+/* isLegendaryRun adalah mekanisme jika memilih run dari Legendary Tokemon */
 isLegendaryRun(Id) :-
     randomRun(1,6,R),
-    ((R =:= 1 -> write('Lucky you, you successfully escaped the Legendary Tokemon!'),
-    retract(command(inittokemonappear,1)),assertz(command(inittokemonappear,0)));
-    write('Poor you, you had to fight the Legendary Tokemon!'),nl,fightLegend(Id),fight),!.
+    /* Peluangnya 1:5 */
+    (
+        /* Mekanisme jika berhasil run */
+        (R =:= 1 -> 
+            write('Lucky you, you successfully escaped the Legendary Tokemon!'),
+            retract(command(inittokemonappear,1)),
+            assertz(command(inittokemonappear,0))
+        );
+        /* Mekanisme jika gagal run */
+            write('Poor you, you had to fight the Legendary Tokemon!'),nl,
+            fightLegend(Id),
+            fight
+    ),!.
